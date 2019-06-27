987d5c24e587767374cdd61859be5e2581d5a670
Arcady Sigov <48837966+ArcSigov@users.noreply.github.com>
2019-06-27 11:47:55 +0300

    Add files via upload
    исправил формирование времени для принудительного блока, [[maybe_unused]] вместо предыдущей реализации передачи параметров

--- bulk.hpp
+++ bulk.hpp
@@ -1,130 +1,33 @@
 #include <vector>
+#include <string>
 #include <tuple>
-#include <chrono>
+#include <cstdlib>
 #include <fstream>
+#include <chrono>
 
 class bulk_handler
 {    
 public:
-    virtual void handler_run(const std::tuple<std::vector<std::string>,unsigned long long> &tuple) = 0;
+    virtual void handler_run(const std::vector<std::string>& vstr,[[maybe_unused]] const unsigned long long &time) = 0;
 };
+
+
 
 class bulk
 {
+    using system_time = std::chrono::time_point<std::chrono::system_clock>;
     std::vector<bulk_handler*> _hndl;
-private:
-   auto get_dynamic_block()
-    {
-        auto line_protector = 1;
-        static auto protector = 0;
-        auto dynamic = 1;
-        std::string line {'\n'};
-        std::string output;
-        while (1)
-        {
-           if(std::getline(std::cin,line))
-           {
-                if (line == "{")
-                {
-                    line_protector ++;
-                }
-                else if (line == "}")
-                {
-                    if (line_protector)
-                        line_protector--;
-                    
-                    if (!line_protector)
-                    {
-                         if (output.size())
-                         {
-                             output.erase(output.find_last_of(","));
-                             subs.push_back(output);
-                         }
-                         dynamic = 0;
-                         break;
-                    }
-                }
-                else
-                {
-                    output+=line+=", ";
-                }       
-           }
-           else
-           { 
-               protector = 1;
-               break;
-           }
-        }
-        return std::make_tuple(dynamic,protector);
-    }
-    auto run_bulk()
-    {  
-        static auto protector = 0;
-        auto dynamic = 0;
-        for (unsigned int i = 0; i < bulk_size;i++)
-        {
-            std::string line;
-            auto start_time = std::chrono::system_clock::now();
-            if (std::getline(std::cin,line))
-            {
-                if (i==0)
-                    time = std::chrono::duration_cast<std::chrono::seconds>(start_time.time_since_epoch()).count();
-        
-                if (line == "{")
-                {
-                    dynamic = 1;
-                    break;
-                }
-                else if (line != "}")
-                {
-                    subs.push_back(line);
-                }
-            }
-            else
-            {
-                protector = 1;
-                break;
-            }
-            
-        }
-        return std::make_tuple(dynamic,protector);
-    }
-    void notify()
-    {
-        for (const auto& it : _hndl)
-            it->handler_run(std::make_tuple(subs,time));
-    }
 public:
-    bulk(int _bulk):bulk_size(_bulk)
-    {
-        subs.reserve(bulk_size);
-    };
-    void add_handler(bulk_handler* _new)
-    {
-        _hndl.emplace_back(_new);
-    }
-    void start()
-    {
-        auto protector = 0;
-        auto dynamic = 0;
-        while (!protector)
-        {
-            subs.clear();
-            if (dynamic)
-            {
-                std::tie(dynamic,protector) = get_dynamic_block();
-            }
-            else
-            {
-                std::tie(dynamic,protector)  = run_bulk();
-            }
-            if (subs.size())
-                notify();
-        }
-    }
+    explicit bulk(int _bulk):bulk_size(_bulk), time(0){};
+    void start();
+    void add_handler(bulk_handler* _new);
 private:
+    void notify();
+    auto run_bulk();
+    auto get_dynamic_block();
     unsigned int bulk_size;
     unsigned long long time;
+    system_time sys_time;
     std::vector<std::string> subs;
 };
 
@@ -136,16 +39,13 @@ public:
     {
         _bulk->add_handler(this);
     }
-    void handler_run (const std::tuple<std::vector<std::string>,unsigned long long> &tuple) override
+    void handler_run(const std::vector<std::string>& vstr, [[maybe_unused]] const unsigned long long &time) override
     {
-        std::vector<std::string> vectr;
-        unsigned long long cur_time = 0;
-        std::tie(vectr,cur_time) = tuple;
-        std::ofstream output("bulk" + std::to_string(cur_time) +".log");
+        std::ofstream output("bulk" + std::to_string(time) +".log");
         output << "bulk: ";
-        for (auto it = vectr.cbegin() ; it !=vectr.cend();it++)
+        for (auto it = vstr.cbegin() ; it !=vstr.cend();it++)
         {
-            if (it != vectr.cbegin())
+            if (it != vstr.cbegin())
                 output << ", ";
             output << *it;
         }
@@ -162,17 +62,136 @@ public:
     {
         _bulk->add_handler(this);
     }
-    void handler_run (const std::tuple<std::vector<std::string>,unsigned long long> &tuple) override
+    void handler_run(const std::vector<std::string>& vstr,[[maybe_unused]] const unsigned long long &time) override
     {
-        std::vector<std::string> vectr;
-        std::tie(vectr,std::ignore) = tuple;
         std::cout << "bulk: ";
-        for (auto it = vectr.cbegin() ; it !=vectr.cend();it++)
+        for (auto it = vstr.cbegin() ; it !=vstr.cend();it++)
         {
-            if (it != vectr.cbegin())
+            if (it != vstr.cbegin())
                 std::cout << ", ";
             std::cout << *it;
         }
         std::cout << std::endl;
     }
 };
+
+
+auto bulk::run_bulk()
+{  
+    static auto protector = 0;
+    auto dynamic = 0;
+    for (unsigned int i = 0; i < bulk_size;i++)
+    {
+        std::string line;
+        if (std::getline(std::cin,line))
+        {
+            if (line == "{")
+            {
+                dynamic = 1;
+                break;
+            }
+            else if (line != "}")
+            {
+                if (i==0)
+                {
+                    time = std::chrono::duration_cast<std::chrono::seconds>(sys_time.time_since_epoch()).count();
+                }
+                subs.push_back(line);
+            }
+        }
+        else
+        {
+            protector = 1;
+            break;
+        }
+        
+    }
+    return std::make_tuple(dynamic,protector);
+}
+
+auto bulk::get_dynamic_block()
+{
+    auto line_protector = 1;
+    static auto protector = 0;
+    auto dynamic = 1;
+    static auto first = 1;
+    std::string line {'\n'};
+    std::string output;
+    while (1)
+    {
+        if(std::getline(std::cin,line))
+        {
+            if (line == "{")
+            {
+                line_protector ++;
+            }
+            else if (line == "}")
+            {
+                if (line_protector)
+                    line_protector--;
+                
+                if (!line_protector)
+                {
+                        if (output.size())
+                        {
+                            output.erase(output.find_last_of(","));
+                            subs.push_back(output);
+                        }
+                        dynamic = 0;
+                        first = 1;
+                        break;
+                }
+            }
+            else
+            {
+                if (first)
+                {
+                    time = std::chrono::duration_cast<std::chrono::seconds>(sys_time.time_since_epoch()).count();
+                    first = 0;
+                }
+                output+=line+=", ";
+            }       
+        }
+        else
+        { 
+            protector = 1;
+            break;
+        }
+    }
+    return std::make_tuple(dynamic,protector);
+}
+
+void bulk::notify()
+{
+    for (const auto& it : _hndl)
+        it->handler_run(subs,time);
+}
+
+
+void bulk::add_handler(bulk_handler* _new)
+{
+    _hndl.emplace_back(_new);
+}
+
+void bulk::start()
+{
+    auto protector = 0;
+    auto dynamic = 0;
+    while (!protector)
+    {
+        sys_time = std::chrono::system_clock::now();
+        subs.clear();
+        if (dynamic)
+        {
+            std::tie(dynamic,protector) = get_dynamic_block();
+        }
+        else
+        {
+            std::tie(dynamic,protector)  = run_bulk();
+        }
+        if (subs.size())
+        {
+            notify();
+        }
+    }
+}
